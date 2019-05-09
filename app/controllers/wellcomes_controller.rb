58718a05a649b955f0a85c15534e6f70404ddc70
endyranaudo <endyranaudo@gmail.com>
2019-05-09 18:50:49 +0100

    need to add validations

--- app/controllers/wellcomes_controller.rb
+++ app/controllers/wellcomes_controller.rb
@@ -1,6 +1,8 @@
 class WellcomesController < ApplicationController
 
   def welcome
+    @total_users = User.all.count
+    @total_ads = Sale.all.count + Rental.all.count
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,16 +21,17 @@ u5 = User.create(username: "Renegade", first_name: "Lorenzo", last_name: "Lamas"
 
 sale1 = Sale.create(title: "Fender Jaguar", description:"Fender Jaguar 1969 looking amazing!", photo_url:"https://d1aeri3ty3izns.cloudfront.net/media/37/372569/1200/preview.jpg", price:"1500")
 sale2 = Sale.create(title: "Ibanez JS1000", description:"Ibanez JS1000 in good conditions", photo_url:"https://pbs.twimg.com/media/DMzeuynVQAA8Oyn.jpg", price:"1200")
-sale3 = Sale.create(title: "Gibson SG", description:"Gibson SG red", photo_url:"...", price:"3000")
-sale4 = Sale.create(title: "Martin D28", description:"Martin D28 vintage, good overall conditions", photo_url:"...", price:"75")
-sale5 = Sale.create(title: "Gibson SJ200", description:"Gibson SJ200. Home use only", photo_url:"...", price:"85")
+sale3 = Sale.create(title: "Gibson SG", description:"Gibson SG red", photo_url:"https://media.sweetwater.com/api/i/q-82__ha-91b7bb8d72672382__hmac-cc630e36a7e9eb88afec29a6b23face817d1cdd5/images/items/750/SGS6119VENH-large.jpg", price:"3000")
+sale4 = Sale.create(title: "Martin D28", description:"Martin D28 vintage, good overall conditions", photo_url:"https://andertons-productimages.imgix.net/282556-1530186278494.png?w=680&h=680&fit=fill&bg=FFFFFF&auto=compress&auto=format", price:"75")
+sale5 = Sale.create(title: "Gibson SJ200", description:"Gibson SJ200. Home use only", photo_url:"https://media.sweetwater.com/api/i/q-82__ha-0c81edd6ad607a42__hmac-e9e6ac89871204ac8e6bad4e76f32e89b24160bd/images/items/750/SJ20VSG19-large.jpg", price:"85")
 
 
-rental6 = Rental.create(title: "Marshall Plexi", description:"Marshall Plexi - studio use only", photo_url:"https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/p35398g-f750c5e55acdf82a900812ccb31df435.jpg", rental_availability:"12/02/2019", price:"50")
-rental2 = Rental.create(title: "Fender Deluxe", description:"Fender Deluxe vintage, all original", photo_url:"...", rental_availability:"13/04/2019", price:"80")
-rental3 = Rental.create(title: "Eden bass amp", description:"Eden bass amp with cabinet", photo_url:"...", rental_availability:"22/08/2019", price:"70")
+
+rental2 = Rental.create(title: "Fender Deluxe", description:"Fender Deluxe vintage, all original", photo_url:"https://media.sweetwater.com/images/items/750/65DeluxeRev-large.jpg?v=af1d6c5a0d50f7bf", rental_availability:"13/04/2019", price:"80")
+rental3 = Rental.create(title: "Eden bass amp", description:"Eden bass amp with cabinet", photo_url:"https://medias.audiofanzine.com/images/normal/eden-electronics-wtx-series-wtx-500-295321.jpg", rental_availability:"22/08/2019", price:"70")
 rental4 = Rental.create(title: "Guitar Stand", description:"Guitar Stand Proel in aluminium", photo_url:"https://images-na.ssl-images-amazon.com/images/I/819lncuw6IL._SL1500_.jpg", rental_availability:"24/08/2019", price:"10")
 rental5 = Rental.create(title: "Strobe light", description:"Strobe light for your DJ sessions", photo_url:"https://images.samash.com/sa/ABI/ABIGSTLII-P.fpx?cvt=jpg", rental_availability:"26/08/2019", price:"20")
+rental6 = Rental.create(title: "Marshall Plexi", description:"Marshall Plexi - studio use only", photo_url:"https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/p35398g-f750c5e55acdf82a900812ccb31df435.jpg", rental_availability:"12/02/2019", price:"50")
 
 sale_contract1 = SaleContract.create(sale_id: 1, user_id: 2)
 sale_contract2 = SaleContract.create(sale_id: 2, user_id: 3)
